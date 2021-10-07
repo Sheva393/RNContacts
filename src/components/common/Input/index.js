434848bbc6417a5367b096c0cc827ad83dbf3c22
Sheva393 <sheva961@gmail.com>
2021-10-07 20:53:21 +0300

    addCustomInput

---
+++ src/components/common/Input/index.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import {color} from 'react-native-reanimated';
+import colors from '../../../assets/theme/colors';
+import styles from '../Input/styles';
+
+const Input = ({
+  value,
+  onChangeText,
+  style,
+  label,
+  icon,
+  iconPosition,
+  error,
+  ...props
+}) => {
+  const [focused, setFocused] = useState(false);
+
+  const getFlexDirection = () => {
+    if (icon && iconPosition) {
+      if (iconPosition === 'left') {
+        return 'row';
+      } else if (iconPosition === 'right') {
+        return 'row-reverse';
+      }
+    }
+  };
+
+  const getBorderColor = () => {
+    if (focused) {
+      return colors.primary;
+    }
+
+    if (error) {
+      return colors.danger;
+    } else {
+      return colors.grey;
+    }
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      {label && <Text>{label}</Text>}
+      <View
+        style={[
+          styles.wrapper,
+          {alignItems: icon ? 'center' : 'baseline'},
+          {borderColor: getBorderColor(), flexDirection: getFlexDirection()},
+        ]}>
+        <View>{icon && icon}</View>
+        <TextInput
+          style={[styles.textInput, style]}
+          onChangeText={onChangeText}
+          value={value}
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onBlur={() => {
+            setFocused(false);
+          }}
+          {...props}
+        />
+      </View>
+      {error && <Text style={styles.error}>{error}</Text>}
+    </View>
+  );
+};
+
+export default Input;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Container from '../../components/common/Container';
+import Input from '../../components/common/Input/index';
 
 const Login = () => {
-    return (
-      <View>
-        <Text>Login</Text>
-      </View>
-    );
-  };
-  
+  const [value, onChangeText] = React.useState('');
+  return (
+    <Container>
+      <SafeAreaView>
+        <Input
+          label="Username"
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          iconPosition="right"
+          icon={<Text></Text>}
+
+          // error={'This field is required'}
+        />
+        <Input
+          label="Password"
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          icon={<Text>HIDE</Text>}
+          iconPosition="right"
+        />
+      </SafeAreaView>
+    </Container>
+  );
+};
 
 export default Login;
